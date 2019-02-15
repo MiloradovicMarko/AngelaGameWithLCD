009b1b4f0b1cdd6ca754ab98632242714de9082e
Marko <marko.miloradovic@barsanti.edu.it>
2019-02-15 09:49:17 +0100

    15/02/19 Angela Game
    Introduzione dei "casi",ultimazione della impostazione che stampa sul LCD

--- AngelaGameWithLCD.ino
+++ AngelaGameWithLCD.ino
@@ -1,60 +1,128 @@
 #include <LiquidCrystal.h>
-const int btn1 = 2;
-const int btn2 = 3;
-const int btn3 = 4;
-int turno = 0;
-int somma = 0;
-int meta = 0;
-int giocataPre = 0;
-bool turnoStart = true;
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
-
+int btn1 = 2;
+int btn2;
+int btn3;
+int turno;
+int somma;
+int meta;
+int giocataPre;
+bool turnoStart;
+int t;
+int incremento;
 void setup() {
   // put your setup code here, to run once:
+incremento = 0;
+btn1 = 2;
+btn2 = 3;
+btn3 = 4;
+turno = 0;
+somma = 0;
+meta = 0;
+giocataPre = 0;
+turnoStart = true;
+t = 500;
 pinMode(btn1,INPUT_PULLUP);
 pinMode(btn2,INPUT_PULLUP);
 pinMode(btn3,INPUT_PULLUP);
-lcd.begin(0,0);
+lcd.begin(16,2); •
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
 
+void caso0(int giocata)
+{ 
+      if(giocata == 0)
+      {
+        turno = -1;
+        lcd.println(giocata);sistemaCursore(0,0);delay(t);
+        lcd.println("Gioca il secondo giocatore");
+      }
+      else
+      {
+        giocataPre = giocata;
+        somma += giocata;
+        turno = -1;
+        lcd.println(giocata);sistemaCursore(0,0);delay(t);
+        lcd.println("Punteggio raggiunto: ");sistemaCursore(0,0);delay(t);
+        lcd.println(somma);sistemaCursore(0,0);delay(t);
+      }
 
-
-
+}
 void caso1(int giocata)
 {
       if(giocata == 1 || giocata == 6 || giocata == 0 && giocata > 6)
-      { Serial.println("Non barare");attesa() ;} 
+      { 
+        lcd.println("Non barare");attesa() ;} 
       else
       {
         somma += giocata;
         giocataPre = giocata;
-        Serial.println(giocata);
-        Serial.println("Punteggio raggiunto: ");
-        Serial.println(somma);
+        lcd.println(giocata);sistemaCursore(0,0);delay(t);
+        lcd.println("Punteggio raggiunto: ");sistemaCursore(0,0);delay(t);
+        lcd.println(somma);sistemaCursore(0,0);delay(t);
         switchTurno();   
       }
       
 }
+void caso2(int giocata)
+{
+      if(giocata == 2 || giocata == 5 || giocata == 0 && giocata > 6)
+      { 
+        lcd.println("Non barare");attesa() ;} 
+      else
+      {
+        somma += giocata;
+        giocataPre = giocata;
+        lcd.println(giocata);sistemaCursore(0,0);delay(t);
+        lcd.println("Punteggio raggiunto: ");sistemaCursore(0,0);delay(t);
+        lcd.println(somma);sistemaCursore(0,0);delay(t);
+        switchTurno();   
+      }
+      
+}
+void caso3(int giocata)
+{
+      if((giocata == 3 || giocata == 4 || giocata == 0)&& giocata > 6)
+      { lcd.println("Non barare");attesa();} 
+      else
+      {
+        somma += giocata;
+        giocataPre = giocata;
+        lcd.println(giocata);sistemaCursore(0,0);delay(t);
+        lcd.println("Punteggio raggiunto: ");sistemaCursore(0,0);delay(t);
+        lcd.println(somma);sistemaCursore(0,0);delay(t);
+        switchTurno();
+      }
+}
+void caso4(int giocata)
+{
+    giocataPre = giocata;
+    somma += giocata;
+    turno = -1;
+    lcd.println(giocata);sistemaCursore(0,0);delay(t);
+    lcd.println("Punteggio raggiunto: ");sistemaCursore(0,0);delay(t);
+    lcd.println(somma);sistemaCursore(0,0);delay(t);
+    switchTurno();
+}
 void cambioMeta()
 {
   attesa();
+  sistemaCursore(0,1);
   if(digitalRead(btn1) == LOW)
   {
-    lcd.clear();
-    lcd.setCursor(0,1);
     meta++;
+    if(meta > 99)
+    {meta = 30;}
     lcd.print(meta);
   }
   else if(digitalRead(btn3) == LOW)
   {
-    lcd.clear();
-    lcd.setCursor(0,1);
     meta--;
+    if(meta < 30)
+    { meta = 99; }
     lcd.print(meta);
   }
 }
@@ -64,40 +132,41 @@ void attesa()
     {}
 }
 void switchTurno()
-{
+{ 
+    sistemaCursore(0,0);
     if(turno == -1)
-    {turno = 1;lcd.println("Gioca il primo");}
+    {turno = 1;;lcd.println("Gioca il primo");}
     else if(turno == 1)
     {turno = -1;lcd.println("Gioca il secondo");}
 }
 void winOrLose()
 {
+  sistemaCursore(0,0);
   if((somma == meta && turno == 1) || (somma >= meta && turno == 1))
   {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.println("Ha vinto il primo giocatore!");ricomincia();
-    ricomincia();
+    lcd.println("Ha vinto il primo giocatore!");setup();
   }
   else if((somma == meta && turno == -1) || (somma >= meta && turno == -1))
   {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.println("Ha vinto il secondo giocatore!");ricomincia();
+    lcd.println("Ha vinto il secondo giocatore!");setup();
   }
-}  
-void ricomincia()
+}
+void sistemaCursore(int p1,int p2)
+{ lcd.clear();lcd.setCursor(p1,p2); }  
+void setGiocata()
 {
-  lcd.clear();
-  lcd.setCursor(2,0);
-  lcd.println("Per ricominciare premere un qualsiasi bottone");
   attesa();
-  if(digitalRead(btn1) == LOW || digitalRead(btn2) == LOW || digitalRead(btn3) == LOW)
+  sistemaCursore(0,1);
+  if(digitalRead(btn1) == LOW)
   {
-    lcd.println("Punteggio riavviato");
-    turno = 0;
-    somma = 0;
-    meta = 0;
-    turnoStart = true;
+    incremento++;
+    sistemaCursore(0,1);
+    lcd.print(incremento);
+  }
+  else if(digitalRead(btn3) == LOW)
+  {
+    incremento--;
+    sistemaCursore(0,1);
+    lcd.print(incremento);
   }
 }
