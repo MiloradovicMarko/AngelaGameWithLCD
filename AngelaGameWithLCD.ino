6242d69ab5b789986ba7d090ae2a734e8edebb71
Marko <marko.miloradovic@barsanti.edu.it>
2019-05-09 22:44:57 +0200

    Angela Game,ultimato
    A causa di una serie di glitch e bug presenti nella versione precedente,è stato ritenuto opportuno verificare dove tali errori esistevano e risolverli

--- AngelaGameWithLCD.ino
+++ AngelaGameWithLCD.ino
@@ -1,8 +1,8 @@
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
-const int btn1 = 4;
-const int btn2 = 3;
-const int btn3 = 2;
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
+const int btn1 = 11;
+const int btn2 = 13;
+const int btn3 = 12;
 int turno;
 int somma;
 int meta;
@@ -23,6 +23,12 @@ void setup() {
   pinMode(btn2, INPUT_PULLUP);
   pinMode(btn3, INPUT_PULLUP);
   lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("  ANGELA  GAME  ");
+  lcd.setCursor(0,1);
+  lcd.print(" Fatto da Marko");
+  delay(t*3);
+  lcd.clear();
 }
 
 void loop() {
@@ -65,32 +71,35 @@ int assegnaValore()
     primaVolta = false;
     decisioneMeta == false;
   }
-  ritardo();
   while (digitalRead(btn2) == HIGH)
   {
-    if (digitalRead(btn1) == LOW)
-    {
-      ritardo();
-      if (giocata > 0)
-      {
-        lcd.setCursor(0, 0);
-        lcd.print("Giocata corrente");
-        giocata--;
-        lcd.setCursor(0, 1);
-        lcd.print(String(giocata));
-      }
-    }
     if (digitalRead(btn3) == LOW)
     {
       ritardo();
-      if (giocata < 6)
+      giocata++;
+      if (giocata > 6)
       {
-        lcd.setCursor(0, 0);
-        lcd.print("Giocata corrente");
-        giocata++;
-        lcd.setCursor(0, 1);
-        lcd.print(String(giocata));
+        giocata = 0;
       }
+       lcd.setCursor(0, 0);
+       lcd.print("Giocata corrente");
+       lcd.setCursor(0, 1);
+       lcd.print(String(giocata));
+       lcd.setCursor(1,1),lcd.print("                         ");  
+    }
+    if (digitalRead(btn1) == LOW)
+    {
+      ritardo();
+      giocata--;
+      if(giocata < 0)
+      {
+        giocata = 6;
+      }
+      lcd.setCursor(0, 0);
+      lcd.print("Giocata corrente");
+      lcd.setCursor(0, 1);
+      lcd.print(String(giocata));
+      lcd.setCursor(1,1),lcd.print("                         ");  
     }
   }
   return giocata;
@@ -102,14 +111,18 @@ void caso0(int giocata)
 {
   if (giocata == 0)
   {
-    sistemaCursore(0, 0); lcd.print(String(giocata)); delay(t);
+    sistemaCursore(0, 1); 
+    lcd.print(String(giocata)); 
+    lcd.setCursor(0,0);
+    lcd.print("Punteggio Totale");
+    delay(t);
   }
   else
   {
     selezioneCorretta(giocata);
     lcd.clear();
   }
-  sistemaCursore(0, 0); lcd.print("Gioca G2"); delay(t);
+  sistemaCursore(4, 0); lcd.print("Gioca G2"); delay(t);
   turno = -1;
   nuovaGiocata();
 }
@@ -118,7 +131,7 @@ void caso0(int giocata)
 void casoEX(int giocata)
 {
   assegnaValore();
-  if (giocataPre == giocata || giocataPre == (7 - giocata))
+  if (giocataPre == giocata || giocataPre == (7 - giocata) ||giocata == 0)
   {
     selezioneErrata(giocata);
   }
@@ -154,7 +167,7 @@ void cambioMeta()
   {
     sistemaCursore(0, 0);
     lcd.print("Selezionare Meta");
-    lcd.setCursor(0, 1);
+    lcd.setCursor(6, 1);
     lcd.print(String(meta));
     while (digitalRead(btn2) == HIGH)
     {
@@ -166,7 +179,7 @@ void cambioMeta()
         {
           meta = 30;
         }
-        lcd.setCursor(0, 1);
+        lcd.setCursor(6, 1);
         lcd.print(String(meta));
       }
       else if (digitalRead(btn1) == LOW)
@@ -177,7 +190,7 @@ void cambioMeta()
         {
           meta = 99;
         }
-        lcd.setCursor(0, 1);
+        lcd.setCursor(6, 1);
         lcd.print(String(meta));
       }
     }
@@ -199,17 +212,25 @@ void switchTurno()
 {
   if (turno == -1)
   {
-    sistemaCursore(0, 0);
+    sistemaCursore(4, 0);
     turno = 1;
     lcd.print("Gioca G1");
     delay(t);
+    lcd.setCursor(0, 0);
+    lcd.print("Giocata corrente");
+    lcd.setCursor(0, 1);
+    lcd.print(String(0));
   }
   else if (turno == 1)
   {
-    sistemaCursore(0, 0);
+    sistemaCursore(4, 0);
     turno = -1;
     lcd.print("Gioca G2");
     delay(t);
+    lcd.setCursor(0, 0);
+    lcd.print("Giocata corrente");
+    lcd.setCursor(0, 1);
+    lcd.print(String(0));
   }
 }
 
@@ -256,9 +277,7 @@ void selezioneCorretta(int giocata)
 
 void selezioneErrata(int giocata)
 {
-  giocataPre = giocata;
-  sistemaCursore(0, 0);
+  sistemaCursore(3, 0);
   lcd.print("Non barare");
   delay(t);
-
 }
